f355d4c29475eb12c020657bfaaaa64bbd5450c7
Clara Freitas <87377777+Claraf94@users.noreply.github.com>
2025-04-01 14:22:38 +0100

    Pom

---
+++ parkingsystem/src/main/java/distsys/parkingsystem/Parkingsystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package distsys.parkingsystem;
+
+/**
+ *
+ * @author dell
+ */
+public class Parkingsystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
